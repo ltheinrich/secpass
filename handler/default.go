019ac6bed78566feecd3c4728b3d5750eea5b1e6
Vincent Lange <vlange@vincents-imac.speedport_w_724v_09011603_05_015>
2018-08-06 21:06:05 +0200

    login

--- handler/default.go
+++ handler/default.go
@@ -16,6 +16,11 @@ var (
 			// split key string by . to get group.key and return value
 			keys := strings.Split(key, ".")
 			return conf.Config[keys[0]][keys[1]]
+		},
+		"lang": func(key string) string {
+			// split key string by . to get language.key and return value
+			keys := strings.Split(key, ".")
+			return (*conf.Lang[keys[0]])[keys[1]]
 		}}
 
 	// define templates
