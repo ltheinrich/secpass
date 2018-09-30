90a276a30d7f1791137011481d5304bdc704feae
Lennart Heinrich <lennart@ltheinrich.de>
2018-09-30 12:51:24 +0200

    optimize code

---
+++ internal/app/handler/register.go
@@ -0,0 +1,72 @@
+package handler
+
+import (
+	"database/sql"
+	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
+
+	"lheinrich.de/lheinrich/secpass/internal/pkg/conf"
+	"lheinrich.de/lheinrich/secpass/internal/pkg/shorts"
+)
+
+// Register function
+func Register(w http.ResponseWriter, r *http.Request) {
+	if conf.Config["web"]["disableRegistration"] == "true" {
+		redirect(w, "/login")
+		return
+	}
+
+	// check logged in and redirect to index if so
+	if checkSession(w, r) != "" {
+		redirect(w, "/")
+		return
+	}
+
+	// output text if equals special
+	special := 0
+
+	// define values
+	name, password := r.PostFormValue("name"), r.PostFormValue("password")
+	repeat, crypter := r.PostFormValue("repeat"), r.PostFormValue("crypter")
+
+	// check for input
+	if name != "" && password != "" && repeat != "" && len(password) >= 8 && len(repeat) >= 8 && crypter != "" {
+		// check whether passwords match
+		if password == repeat {
+			// check whether name already exists
+			var queryName string
+			errQuery := conf.DB.QueryRow(conf.GetSQL("get_name"), name).Scan(&queryName)
+
+			// name does not exist
+			if errQuery == sql.ErrNoRows {
+				// hash password
+				passwordHash, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost+1)
+
+				// insert user into db and add default category
+				_, errExecRegister := conf.DB.Exec(conf.GetSQL("register"), name, string(passwordHash), "", crypter)
+				_, errExecADC := conf.DB.Exec(conf.GetSQL("add_default_category"), name)
+
+				// check for error
+				shorts.Check(errExecRegister)
+				shorts.Check(errExecADC)
+
+				// redirect and return
+				redirect(w, "/login")
+				return
+			}
+
+			// check error
+			shorts.Check(errQuery)
+
+			// name exists, print
+			special = 1
+		} else {
+			// passwords does not match, print
+			special = 2
+		}
+	}
+
+	// execute template
+	shorts.Check(tpl.ExecuteTemplate(w, "register.html", Data{User: "", Lang: getLang(r), Special: special, LoggedOut: true}))
+}
