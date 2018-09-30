dfe5e5d4c4c53b14f104906e6f711d3fbae337a1
Lennart Heinrich <ltheinrich@bitomic.de>
2018-09-30 12:54:56 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 # linux amd64
-GOOS=linux GOARCH=amd64 go build -o $GOPATH/bin/secpass/secpass-linux-amd64 main.go
+GOOS=linux GOARCH=amd64 go build -o $GOPATH/bin/secpass/secpass-linux-amd64 cmd/secpass/secpass.go
 
 # linux arm64
-GOOS=linux GOARCH=arm64 go build -o $GOPATH/bin/secpass/secpass-linux-arm64 main.go
+GOOS=linux GOARCH=arm64 go build -o $GOPATH/bin/secpass/secpass-linux-arm64 cmd/secpass/secpass.go
+
+# linux arm
+GOOS=linux GOARCH=arm go build -o $GOPATH/bin/secpass/secpass-linux-arm cmd/secpass/secpass.go
 
 # windows amd64
-GOOS=windows GOARCH=amd64 go build -o $GOPATH/bin/secpass/secpass-windows-amd64.exe main.go
+GOOS=windows GOARCH=amd64 go build -o $GOPATH/bin/secpass/secpass-windows-amd64.exe cmd/secpass/secpass.go
