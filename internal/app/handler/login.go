421cd068538a7a08fe33f76de8fae1a1b114d760
Lennart Heinrich <ltheinrich@bitomic.de>
2018-09-30 12:51:24 +0200

    optimize code

---
+++ internal/app/handler/login.go
@@ -0,0 +1,99 @@
+package handler
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/pquerna/otp/totp"
+
+	"golang.org/x/crypto/bcrypt"
+
+	"lheinrich.de/lheinrich/secpass/internal/pkg/conf"
+	"lheinrich.de/lheinrich/secpass/internal/pkg/spuser"
+
+	"lheinrich.de/lheinrich/secpass/internal/pkg/shorts"
+)
+
+// Login function
+func Login(w http.ResponseWriter, r *http.Request) {
+	// check logged in and redirect to index if so
+	if checkSession(w, r) != "" {
+		// logout
+		if r.URL.Path == "/login/logout" {
+			// delete session
+			delete(spuser.Sessions, getCookie(r, "secpass_uuid"))
+
+			// delete cookies
+			deleteAllCookies(w)
+
+			// redirect to login page
+			redirect(w, "/login")
+			return
+		}
+
+		// redirect to index
+		redirect(w, "/")
+		return
+	}
+
+	// output login data wrong, crypter key
+	special := 0
+	var crypter string
+
+	// define values
+	name, password := r.PostFormValue("name"), r.PostFormValue("password")
+
+	// check for input
+	if name != "" && password != "" && len(password) >= 8 {
+		// define variables to write into
+		var username string
+		var passwordHash string
+		var twoFactorSecret string
+		var key string
+
+		// read data from database
+		conf.DB.QueryRow(conf.GetSQL("login"), name).Scan(&username, &passwordHash, &twoFactorSecret, &key)
+
+		// compare passwords
+		if bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(password)) == nil {
+			// define two-factor authentication variables
+			oneTimePassword := r.PostFormValue("oneTimePassword")
+			var skipLogin bool
+
+			// check two-factor authentication is enabled
+			if twoFactorSecret != "" {
+				// validate one-time password
+				if (len(oneTimePassword) != 6 && len(oneTimePassword) != 8) || !totp.Validate(oneTimePassword, twoFactorSecret) {
+					// skip log-in and output error
+					skipLogin = true
+					special = 2
+				}
+			}
+
+			if !skipLogin {
+				// generate uuid and define session expiration date
+				uuid := shorts.UUID()
+				expires := time.Now().Add(10 * time.Minute)
+
+				// define cookies
+				cookieUUID := http.Cookie{Name: "secpass_uuid", Value: uuid, Expires: expires}
+				cookieName := http.Cookie{Name: "secpass_name", Value: username, Expires: expires}
+
+				// add session and set cookies
+				spuser.Sessions[uuid] = spuser.Session{User: username, Expires: expires}
+				http.SetCookie(w, &cookieUUID)
+				http.SetCookie(w, &cookieName)
+
+				// send crypter
+				crypter = key
+			}
+		} else {
+			// wrong password
+			special = 1
+		}
+	}
+
+	// execute template
+	shorts.Check(tpl.ExecuteTemplate(w, "login.html", Data{User: "", Lang: getLang(r), Special: special,
+		LoggedOut: true, Crypter: crypter}))
+}
---
+++ internal/pkg/spuser/spuser.go
@@ -0,0 +1,89 @@
+package spuser
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"lheinrich.de/lheinrich/secpass/internal/pkg/conf"
+	"lheinrich.de/lheinrich/secpass/internal/pkg/shorts"
+)
+
+// Sessions map
+var Sessions = map[string]Session{}
+
+// Session structure
+type Session struct {
+	User    string
+	Expires time.Time
+}
+
+// IsValid check session is valid
+func (s Session) IsValid() bool {
+	return s.Expires.Before(time.Now())
+}
+
+// CleanupSessions delete sessions no longer valid
+func CleanupSessions() {
+	for uuid, session := range Sessions {
+		if !session.IsValid() {
+			delete(Sessions, uuid)
+		}
+	}
+}
+
+// PwnedList titles from https://haveibeenpwned.com/api/v2/breachedaccount/<NAME>
+func PwnedList(name string) []string {
+	// http request and check error
+	resp, err := http.DefaultClient.Get("https://haveibeenpwned.com/api/v2/breachedaccount/" + name)
+	shorts.Check(err)
+
+	// read body and check error
+	read, errRead := ioutil.ReadAll(resp.Body)
+	shorts.Check(errRead)
+
+	// struct for json
+	type titleStruct struct {
+		Title string
+	}
+
+	// unmarshal json
+	titleList := []titleStruct{}
+	json.Unmarshal(read, &titleList)
+
+	// append to slice from struct
+	titles := []string{}
+	for _, title := range titleList {
+		titles = append(titles, title.Title)
+	}
+
+	// return
+	return titles
+}
+
+// TwoFactorSecret get two-factor authentication secret and "" if disabled
+func TwoFactorSecret(user string) string {
+	// query db
+	row := conf.DB.QueryRow(conf.GetSQL("get_two_factor_secret"), user)
+
+	// read from query
+	var secret string
+	err := row.Scan(&secret)
+	shorts.Check(err)
+
+	// return
+	return secret
+}
+
+// EnableTwoFactor enable two-factor authentication
+func EnableTwoFactor(user, secret string) {
+	_, err := conf.DB.Exec(conf.GetSQL("enable_two_factor"), secret, user)
+	shorts.Check(err)
+}
+
+// DisableTwoFactor disable two-factor authentication
+func DisableTwoFactor(user string) {
+	_, err := conf.DB.Exec(conf.GetSQL("disable_two_factor"), user)
+	shorts.Check(err)
+}
