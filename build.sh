c1bf2597de00b8ec2e10998f5d2c586fae5e4bd6
Lennart Heinrich <lennart@ltheinrich.de>
2018-10-11 13:14:07 +0200

    typo: GOARM=6

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ GOOS=linux GOARCH=amd64 go build -o $GOPATH/bin/secpass/secpass-linux-amd64 cmd/
 GOOS=linux GOARCH=arm64 go build -o $GOPATH/bin/secpass/secpass-linux-arm64 cmd/secpass/secpass.go
 
 # linux arm
-GOOS=linux GOARCH=arm GOARCH=6 go build -o $GOPATH/bin/secpass/secpass-linux-arm cmd/secpass/secpass.go
+GOOS=linux GOARCH=arm GOARM=6 go build -o $GOPATH/bin/secpass/secpass-linux-arm cmd/secpass/secpass.go
 
 # windows amd64
 GOOS=windows GOARCH=amd64 go build -o $GOPATH/bin/secpass/secpass-windows-amd64.exe cmd/secpass/secpass.go
