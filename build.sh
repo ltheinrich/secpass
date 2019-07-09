0ba0122b1cf42fb0f73cf8436d3967850da3aa45
Lennart Heinrich <lennart@ltheinrich.de>
2019-07-09 13:01:32 +0200

    strip binaries

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 rm -rf target
 mkdir target
-GOOS=linux GOARCH=amd64 go build -o target/secpass-linux-amd64 cmd/secpass/secpass.go
-GOOS=linux GOARCH=arm64 go build -o target/secpass-linux-arm64 cmd/secpass/secpass.go
+GOOS=linux GOARCH=amd64 go build -o target/secpass-linux-x86_64 cmd/secpass/secpass.go
+GOOS=linux GOARCH=arm64 go build -o target/secpass-linux-aarch64 cmd/secpass/secpass.go
 GOOS=linux GOARCH=arm GOARM=7 go build -o target/secpass-linux-armv7 cmd/secpass/secpass.go
-GOOS=windows GOARCH=amd64 go build -o target/secpass-windows-amd64.exe cmd/secpass/secpass.go
+GOOS=windows GOARCH=amd64 go build -o target/secpass-windows-x86_64.exe cmd/secpass/secpass.go
+strip target/secpass-linux-x86_64
+aarch64-linux-gnu-strip target/secpass-linux-aarch64
+arm-linux-gnueabihf-strip target/secpass-linux-armv7
+x86_64-w64-mingw32-strip target/secpass-windows-x86_64.exe
 tar cfJ target/resources.tar.xz resources
