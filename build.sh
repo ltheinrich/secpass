273649f4a09fb5af7a5342bfb1d052651921bc09
Lennart Heinrich <lennart@ltheinrich.de>
2019-07-09 12:55:24 +0200

    better build script

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-GOOS=linux GOARCH=amd64 go build -o $GOPATH/bin/secpass/secpass-linux-amd64 cmd/secpass/secpass.go
-GOOS=linux GOARCH=arm64 go build -o $GOPATH/bin/secpass/secpass-linux-arm64 cmd/secpass/secpass.go
-GOOS=linux GOARCH=arm GOARM=7 go build -o $GOPATH/bin/secpass/secpass-linux-armv7 cmd/secpass/secpass.go
-GOOS=windows GOARCH=amd64 go build -o $GOPATH/bin/secpass/secpass-windows-amd64.exe cmd/secpass/secpass.go
+rm -rf target
+mkdir target
+GOOS=linux GOARCH=amd64 go build -o target/secpass-linux-amd64 cmd/secpass/secpass.go
+GOOS=linux GOARCH=arm64 go build -o target/secpass-linux-arm64 cmd/secpass/secpass.go
+GOOS=linux GOARCH=arm GOARM=7 go build -o target/secpass-linux-armv7 cmd/secpass/secpass.go
+GOOS=windows GOARCH=amd64 go build -o target/secpass-windows-amd64.exe cmd/secpass/secpass.go
+tar cfJ target/resources.tar.xz resources
