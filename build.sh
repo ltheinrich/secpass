0b5b7d9bfda57de72f9ff407e6a04a62d2bef47d
Lennart Heinrich <lennart@ltheinrich.de>
2019-08-14 13:14:46 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 rm -rf target
 mkdir target
-GOOS=linux GOARCH=amd64 go build -o target/secpass-linux-x86_64 cmd/secpass/secpass.go
-GOOS=linux GOARCH=arm64 go build -o target/secpass-linux-aarch64 cmd/secpass/secpass.go
-GOOS=linux GOARCH=arm GOARM=7 go build -o target/secpass-linux-armv7 cmd/secpass/secpass.go
-GOOS=windows GOARCH=amd64 go build -o target/secpass-windows-x86_64.exe cmd/secpass/secpass.go
+GOOS=linux GOARCH=amd64 go build -o target/secpass-linux-x86_64
+GOOS=linux GOARCH=arm64 go build -o target/secpass-linux-aarch64
+GOOS=linux GOARCH=arm GOARM=7 go build -o target/secpass-linux-armv7
+GOOS=windows GOARCH=amd64 go build -o target/secpass-windows-x86_64.exe
 strip target/secpass-linux-x86_64
 aarch64-linux-gnu-strip target/secpass-linux-aarch64
 arm-linux-gnueabihf-strip target/secpass-linux-armv7
